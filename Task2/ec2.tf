052e1f25d70eb66a329a3e695c6dc1e3d9a2f9b9
AndronovDanilo <azumalive@gmail.com>
2026-06-09 08:59:32 +0300

    Task2 v7

--- Task2/ec2.tf
+++ Task2/ec2.tf
@@ -17,8 +17,18 @@ data "aws_security_group" "security_group" {
   name   = var.security_group_name
 }
 
+data "aws_ami" "latest_amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 resource "aws_instance" "cmtr-ygbit6f1-ec2" {
-  ami                         = "ami-0bb84b8f0cc4f36c5"
+  ami                         = data.aws_ami.latest_amazon_linux_2.id
   instance_type               = "t2.micro"
   key_name                    = aws_key_pair.ssh_key.key_name
   subnet_id                   = data.aws_subnet.subnet.id
